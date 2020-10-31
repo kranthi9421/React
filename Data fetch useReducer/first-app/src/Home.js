f9181bb69f62fbefa9f8c27eec0d5fa25844fee4
kranthi <kranthi.007007@gmail.com>
2020-11-01 00:15:46 +0530

    k

---
+++ Data fetch useReducer/first-app/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DataFetchingOne from './DataFetchingOne';
+import DataFetchingTwo from './DataFetchingTwo';
+
+function Home() {
+    return (
+        <div>
+           {/* <DataFetchingOne/>*/}
+            <DataFetchingTwo/>
+        </div>
+    )
+}
+
+export default Home
