9fa72172e83fef594ec1e25f37cd10f5db524ceb
kranthi <kranthi.007007@gmail.com>
2020-11-06 18:15:00 +0530

    k

---
+++ Custom-Hook1/first-app/src/DocTitleTwo.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import useDocumentTitle from './useDocumentTitle'
+
+function DocTitleTwo() {
+  const [count,setCount] = useState(0)
+
+ useDocumentTitle(count)
+
+
+ return (
+        <div>
+            <button onClick={()=> setCount(count + 1)}>Count - {count}</button>
+        </div>
+    )
+}
+
+export default DocTitleTwo
---
+++ Custom-Hook1/first-app/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DocTitleOne from './DocTitleOne';
+import DocTitleTwo from './DocTitleTwo';
+
+function Home() {
+    return (
+        <div>
+            <DocTitleOne/>
+            <DocTitleTwo/>
+        </div>
+    )
+}
+
+export default Home
